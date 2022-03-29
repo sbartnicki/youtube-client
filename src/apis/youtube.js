99df0744ca4f3d45101c76d56642cabf81031def
Sly <sylwesterbartnicki@gmail.com>
2022-03-28 23:38:08 -0400

    refactored app with custom hook

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // Api key removed before pushing to git, update for app to work
-const KEY = '';
+const KEY = 'AIzaSyC67EuzWy8bLKy5CHDu_JxjUgV1EiagEtA';
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
